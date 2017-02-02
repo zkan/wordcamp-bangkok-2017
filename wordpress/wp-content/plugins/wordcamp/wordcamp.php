9f1272c1c9b405db4107f5bf22917f0761f3fc97
zkan <kan@prontomarketing.com>
2017-02-02 14:12:16 +0700

    Add wordcamp plugin for generating credits for Twenty Sixteen theme

---
+++ wordpress/wp-content/plugins/wordcamp/wordcamp.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: WordCamp Hook
+ * Description: A simple plugin to test hooks at WordCamp Bangkok
+ */
+
+function generate_credits() {
+	// Dashicons: https://developer.wordpress.org/resource/dashicons/#wordpress
+	$credits = '<span class="dashicons dashicons-wordpress"></span>';
+	$credits .= '<span class="dashicons dashicons-twitter"></span>';
+
+	echo '<div>';
+	echo apply_filters( 'wordcamp_credits', $credits );
+	echo '</div>';
+}
+add_action( 'twentysixteen_credits', 'generate_credits' );
